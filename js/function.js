48dd08c35e35e18972280f0346b4c53d08516f0c
ec-digit-rsp-mvp <62853393+ec-digit-rsp-mvp@users.noreply.github.com>
2020-07-17 16:20:24 +0200

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	
+	//HAMBURGER//
+	$('.navbar-toggler-animation').on('click', function () {
+
+    $('.hamburger-animation').toggleClass('hamburger-open');
+  });
+	
+	//ACCORDION//
+	$(".collapse.show").each(function(){
+		$(this).prev(".accordion-item-header").find(".ico-accordion").addClass("ico-remove-dark").removeClass("ico-add-dark");
+	});
+
+	$(".collapse").on('show.bs.collapse', function(){
+		$(this).prev(".accordion-item-header").find(".ico-accordion").removeClass("ico-add-dark").addClass("ico-remove-dark");
+	}).on('hide.bs.collapse', function(){
+		$(this).prev(".accordion-item-header").find(".ico-accordion").removeClass("ico-remove-dark").addClass("ico-add-dark");
+	});
+});
+
+
+//HEADER SHRINK//
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("fixed-top").classList.add("fixed-top");
+  } else {
+    document.getElementById("fixed-top").classList.remove("fixed-top");
+  }
+}
+
+
